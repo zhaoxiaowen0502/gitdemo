64f8aaec457abaca6cec7c9bdeb4992e6789b626
zhaoxiaowen <1721207820@qq.com>
2019-02-11 18:34:04 +0800

    test

---
+++ ideas/src/Test/Test1.java
@@ -0,0 +1,45 @@
+package Test;
+
+public class Test1 {
+    public static void quicksort(int[] array, int beginIndex, int endIndex) {
+        if(beginIndex>=endIndex){
+            return;
+        }
+        int MaxIndex = -1;
+        for (int i = beginIndex; i < endIndex; i++) {
+            if (array[i] > array[endIndex]) {
+                if (-1 == MaxIndex) {
+                    MaxIndex = i;
+                }
+            } else {
+                if (MaxIndex != -1) {
+                    int t = array[MaxIndex];
+                    array[MaxIndex] = array[i];
+                    array[i] = t;
+                    MaxIndex++;
+                }
+            }
+        }
+
+        if (-1 == MaxIndex) {
+            quicksort(array, beginIndex, endIndex - 1);
+        } else {
+            int t = array[MaxIndex];
+            array[MaxIndex] = array[endIndex];
+            array[endIndex] = t;
+
+            quicksort(array, beginIndex, MaxIndex - 1);
+            quicksort(array, MaxIndex + 1, endIndex);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] array = {8, 5, 3, 9, 2};
+        Test1.quicksort(array, 0, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+}
